eb8b0fd5b3c92abdbfd512db54de0b3099676b10
Hung Phung <200452314@student.georgianc.on.ca>
2021-10-06 06:54:07 -0400

    Create new Portfolio

---
+++ controllers/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Portfolio' });
+})
+
+// GET: /about
+router.get('/about', (req, res) =>{
+  res.render('about', {
+    title: 'About this Site',
+    content: 'Here is content of about Page'
+  })
+})
+
+// GET: /project
+router.get('/project', (req, res) =>{
+  res.render('project', {
+    title: 'Project this Site',
+    content: 'Here is content of project Page'
+  })
+})
+
+// GET: /contact
+router.get('/contact', (req, res) =>{
+  res.render('contact', {
+    title: 'Contact this Site',
+    content: 'Here is content of contact Page'
+  })
+})
+
+module.exports = router;
