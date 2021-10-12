8204af44832389f16b4006d4382c41f3d9f5c1f0
Hung Phung <200452314@student.georgianc.on.ca>
2021-10-11 20:34:59 -0400

    added footer

--- controllers/index.js
+++ controllers/index.js
@@ -6,5 +6,28 @@ router.get('/', function(req, res, next) {
   res.render('index');
 })
 
+// GET: /about
+router.get('/about', (req, res) =>{
+  res.render('about', {
+    title: 'About this Site',
+    content: 'Here is content of about Page'
+  })
+})
+
+// GET: /project
+router.get('/project', (req, res) =>{
+  res.render('project', {
+    title: 'Project this Site',
+    content: 'Here is content of project Page'
+  })
+})
+
+// GET: /contact
+router.get('/contact', (req, res) =>{
+  res.render('contact', {
+    title: 'Contact this Site',
+    content: 'Here is content of contact Page'
+  })
+})
 
 module.exports = router;
